3b6a602c7291fecb5b3c76e79d3133aeaa59b81d
vankha09052007 <vankha09052007@gmail.com>
2025-03-16 11:38:52 +0700

    Create Frontend food delivery website done

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,15 +3,16 @@ import'./Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
   const [category, setCategory]=useState("All");
   return (
     <div>
-     <Header/>
-     <ExploreMenu category={category} setCategory={setCategory}/>
-     <FoodDisplay category={category}/>
-
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+      <AppDownload/> 
     </div>
   )
 }
